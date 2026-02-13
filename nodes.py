4d6607963342725c97e7666288b1bebb0ef8165e
Bruna <brunavieira@nano.ufrj.br>
2026-02-13 15:55:10 -0300

    feat: implement nodes

---
+++ nodes.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+
+from react import llm, tools
+
+load_dotenv()
+
+SYSTEM_MESSAGE = """
+You are helpful assistant that can use tools to answer questions.
+"""
+
+tool_node = ToolNode(tools)
+
+def run_agent_reasoning(state: MessagesState) -> MessagesState:
+    """
+    Run the agent reasoning node.
+    """
+    response = llm.invoke([{"role": "system", "content": SYSTEM_MESSAGE}, *state["messages"]])
+    return {"messages": [response]}
