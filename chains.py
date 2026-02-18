14c1ac3c9d755287c3ee8febe4a661d5fa285c14
Bruna <brunavieira@nano.ufrj.br>
2026-02-18 17:20:30 -0300

    feat: implement actor agent within the reflexion agent

--- chains.py
+++ chains.py
@@ -1,36 +1,63 @@
+import datetime
+
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser
+)
+from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
+
+from schemas import AnswerQuestion
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-reflection_prompt = ChatPromptTemplate.from_messages(
+llm = ChatOpenAI(model="gpt-4-turbo-preview")
+parser = JsonOutputToolsParser(return_id=True)
+parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
+
+actor_prompt_template = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for user's tweet.\n"
-            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
-        ),
-        MessagesPlaceholder(variable_name="messages")
+            """You are expert researcher.
+                Current time: {time}
+
+                1. {first_instruction}
+                2. Reflect and critique your answer. Be severe to maximize improvement.
+                3. Recommend search queries to research information and improve your answer.
+            """,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+            ("system", "Answer the user's question above using the required format."),
     ]
+).partial(
+    time=lambda: datetime.datetime.now().isoformat(),
+)
+
+first_reponder_prompt_template = actor_prompt_template.partial(
+    first_instruction="Provide a detailed ~250 word answer."
 )
 
 
-generation_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts.\n"
-            "Generate the best twitter post possible for the user's request.\n"
-            "If the user provides critique, respond with a revised version of your previous attempt.\n",
-        ),
-        MessagesPlaceholder(variable_name="messages")
-    ]
+first_reponder = first_reponder_prompt_template | llm.bind_tools(
+    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
 )
 
 
-llm = ChatOpenAI(model="gpt-4o-mini")
-generate_chain = generation_prompt | llm
-reflection_chain = reflection_prompt | llm
+if __name__ == "__main__":
+    human_message = HumanMessage(
+        content="Write about AI-powered SOC / autonomous SOC problem domain,"
+        " list startups that do that and raised capital."
+    )
+    chain = (
+        first_reponder_prompt_template
+        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+        | parser_pydantic
+    )
+    
+    res = chain.invoke(input={"messages": [human_message]})
+    print(res)
