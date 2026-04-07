5098c0fe54e251a65967cf1bd8f35b5d20bdee23
Bruna <brunavieira@nano.ufrj.br>
2026-04-07 16:08:19 -0300

    chore: first commit - project structure

--- main.py
+++ main.py
@@ -1,71 +1,7 @@
-from typing import Literal
+from dotenv import load_dotenv
 
-from langchain_core.messages import AIMessage, ToolMessage
-from langgraph.graph import END, START, StateGraph, MessagesState
-from langgraph.graph.message import add_messages
-
-from chains import revisor, first_responder
-from tool_executor import execute_tools
+load_dotenv()
 
 
-MAX_ITERATIONS = 2
-
-
-def draft_node(state: MessagesState):
-    """Draft the initial response."""
-    response = first_responder.invoke({"messages": state["messages"]})
-    return {"messages": [response]}
-
-def revise_node(state: MessagesState):
-    """Revise the answer based on tool results."""
-    response = revisor.invoke({"messages": state["messages"]})
-    return {"messages": [response]}
-
-def event_loop(state: MessagesState)  -> Literal["execute_tools", END]:
-    """Determine whether to continue or end based on iteration count."""
-    count_tool_visits = sum(
-        isinstance(item, ToolMessage) for item in state["messages"]
-    )
-    if count_tool_visits > MAX_ITERATIONS:
-        return END
-    return "execute_tools"    
-
-builder = StateGraph(MessagesState)
-
-builder.add_node("draft", draft_node)
-builder.add_node("execute_tools", execute_tools)
-builder.add_node("revise", revise_node)
-
-builder.add_edge(START, "draft")
-builder.add_edge("draft", "execute_tools")
-builder.add_edge("execute_tools", "revise")
-
-builder.add_conditional_edges("revise", event_loop, ["execute_tools", END])
-
-graph = builder.compile()
-graph.get_graph().draw_mermaid_png(output_file_path="flow.png")
-print(graph.get_graph().draw_mermaid())
-graph.get_graph().print_ascii()
-
-
-def main():
-    print("Hello, Reflexion Agent!")
-    res = graph.invoke(
-    {
-        "messages": [
-            {
-                "role": "user",
-                "content": "Write about AI-Powered SOC / autonomous soc problem domain, list startups that do that and raised capital.",
-            }
-        ]
-    }
-    )
-    # Extract the final answer from the last message with tool calls
-    last_message = res["messages"][-1]
-    if isinstance(last_message, AIMessage) and last_message.tool_calls:
-        print(last_message.tool_calls[0]["args"]["answer"])
-    print(res)
-    
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    print("Hello, Advanced RAG!")
